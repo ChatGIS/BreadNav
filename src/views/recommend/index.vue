74a7640e8c834bbc07f8ce7bf7cba07804a339e4
Dreamice <dreamice13@foxmail.com>
2022-10-29 22:13:40 +0800

    迁移

---
+++ src/views/recommend/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <h3>推荐页面</h3>
+    <div style="border: 1px solid red; height: 80vh">
+        <iframe
+            style="height: 100%; width: 100%"
+            src="http://10.209.198.83:18080/gisclient/login.html?&login_no=test&system_type=SX_GCSP_NET2&userid=1000&gis_sceneid=10019905&=undefined&layerid=10014074%2C10017495%2C10013289"
+            frameborder="0"
+        ></iframe>
+        <!--            name="ossFrame"-->
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'index',
+}
+</script>
+
+<style scoped></style>
