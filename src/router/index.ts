44b39c5b89e9836ff82aa51a21d160e971919f5a
Dreamice <dreamice13@foxmail.com>
2022-10-31 20:54:32 +0800

    添加导航守卫

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import Recommend from '../views/recommend/index.vue'
 import Resource from '../views/resource/index.vue'
 import My from '../views/my/index.vue'
 
+
 const routes = [{
     path: '/login',
     component: () => import('../views/login.vue')
@@ -11,7 +12,13 @@ const routes = [{
     component: () => import('../views/container.vue'),
     children: [
         { path: '/recommend', component: Recommend },
-        { path: '/resource', component: Resource },
+        { 
+            path: '/resource', 
+            component: Resource,
+            meta: {
+                requiresAuth: true
+            }
+        },
         // { path: '/my', component: My },
     ]
 }]
@@ -21,4 +28,16 @@ const router = createRouter({
     routes,
 })
 
+// 导航守卫
+router.beforeEach((to, from) => {
+    if (to.meta.requiresAuth && !window.localStorage.getItem('mbToken')) {
+        // 此路由需要授权，请检查是否已登录
+        // 如果没有，则重定向到登录页面
+        return {
+            path: '/login',
+            // 保存我们所在的位置，以便以后再来
+            query: { redirect: to.fullPath },
+        }
+    }
+})
 export default router
