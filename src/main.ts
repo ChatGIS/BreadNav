8a94c56d004c108a9240328751a24a83fc7605e7
Dreamice <dreamice13@foxmail.com>
2022-11-12 19:52:23 +0800

    ElementUiPlue升级，使用element icon

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,16 @@ import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import router from './router'
 import './assets/styles/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 // 创建pinia实例
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
 
 const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
 app.use(pinia) // pinia挂载到 vue 根实例
     .use(router)
     .mount('#app')
