8ee9b9c33b8e82fd5b00102875512eaf30641b5c
Dreamice <dreamice13@foxmail.com>
2022-12-26 21:40:36 +0800

    设置路径别名 @

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import eslint from 'vite-plugin-eslint'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,4 +21,10 @@ export default defineConfig({
             include: ['src/**/*.js', 'src/**/*.vue', 'src/*.js', 'src/*.vue']
         }),
     ],
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, 'src'), // 路径别名
+        },
+        extensions: ['.js', '.json', '.ts'] // 使用路径别名时想要省略的后缀名，可以自己 增减
+    }
 })
